b0116b1ab64c2a519deaefa04bf7307c76a01e5a
Stealth2994 <cnnr252@gmail.com>
2016-09-17 15:58:59 -0400

    pooling untracked

---
+++ New Unity Project/Assets/Generation Scripts/PoolSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class PoolSystem : MonoBehaviour {
+
+    public GameObject pooledObject;
+    public int pooledAmount = 20;
+    public bool willGrow = true;
+    public int code;
+    public List<GameObject> pooledObjects;
+
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledObject);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (pooledObjects[i] == null)
+            {
+                GameObject obj = (GameObject)Instantiate(pooledObject);
+                obj.SetActive(false);
+                pooledObjects[i] = obj;
+                return pooledObjects[i];
+            }
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        if (willGrow)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledObject);
+            pooledObjects.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+    public void RecycleObject(GameObject g)
+    {
+        pooledObjects.Add(g);
+        g.SetActive(false);
+    }
+}
