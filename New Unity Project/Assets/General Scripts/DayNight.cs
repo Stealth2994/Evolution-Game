c1492487eed8602c844e196ac8b676af81f7e910
Caelan Douglas <caelan.douglas@outlook.com>
2016-09-17 23:38:05 -0400

    Day and night cycle added

---
+++ New Unity Project/Assets/General Scripts/DayNight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DayNight : MonoBehaviour {
+
+	bool TimeIsZero;
+	public Image DarknessImage;
+	public float secondsInFullDay = 120f;
+	[Range(0,1)]
+	public float currentTimeOfDay = 0;
+	public float timeMultiplier = 1f;
+
+	void Update() {
+
+		if (TimeIsZero == true) {
+			currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
+			if (currentTimeOfDay >= 1) {
+				TimeIsZero = false;
+			}
+		} else if (TimeIsZero == false) {
+			currentTimeOfDay -= (Time.deltaTime / secondsInFullDay) * timeMultiplier;
+			if (currentTimeOfDay <= 0) {
+				TimeIsZero = true;
+			}
+		}
+		if (currentTimeOfDay <= 0.3f) {
+			timeMultiplier = 0.5f;
+		} else {
+			timeMultiplier = 1f;
+		}
+			DarknessImage.color = new Color (0, 0, 0, currentTimeOfDay);
+	}
+}
