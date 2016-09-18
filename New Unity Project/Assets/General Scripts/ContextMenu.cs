b17a3da6a9461590e5c1dfa746a565e37913b284
Caelan Douglas <caelan.douglas@outlook.com>
2016-09-18 01:07:05 -0400

    Added hunger, wheat context menu, and more

---
+++ New Unity Project/Assets/General Scripts/ContextMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContextMenu : MonoBehaviour {
+
+	public GameObject ContextSprite;
+
+	void OnMouseDown () {
+		if (ContextSprite.activeSelf == false) {
+			ContextSprite.SetActive (true);
+		} else if (ContextSprite.activeSelf == true) {
+			ContextSprite.SetActive (false);
+		}
+	}
+}
---
+++ New Unity Project/Assets/Player Scripts/PlayerHunger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHunger : MonoBehaviour {
+
+	float totalHunger = 100f;
+	[Range(0,1)]
+	[HideInInspector]
+	public float currentHunger = 1f;
+	public float depletionRate = 1f;
+	public Text HungerText;
+
+	void Update () {
+		currentHunger -= (Time.deltaTime / totalHunger) * depletionRate;
+		HungerText.text = ("H: " + Mathf.Round (currentHunger * 100));
+
+		if (currentHunger > 1f) {
+			currentHunger = 1f;
+		}
+	}
+}
