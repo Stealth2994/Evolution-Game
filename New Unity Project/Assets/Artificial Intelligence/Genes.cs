191f28283cf0a74db09482b1632b20105e743d13
Connor Adams <cnnr252@gmail.com>
2016-10-05 16:30:43 -0400

    Basic Genes, goes on every AI and maybe even the player

---
+++ New Unity Project/Assets/Artificial Intelligence/Genes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Genes : MonoBehaviour {
+    //Stores all of the genes/traits of the current AI
+
+    //Priority Weights
+    public int foodPriority;
+    public int waterPriority;
+    public int breedPriority;
+    public int restPriority;
+
+    //Trait Weights
+    public int fleeLevel;
+    public int fightLevel;
+    public int camouflageLevel;
+
+    //Physical Attributes
+    public int speed;
+    public int energy;
+    public int health;
+    public int sprintSpeed;
+    public int hitDamage;
+    public int camouflageSkill;
+    public int swimSkill;
+    public int landSkill;
+}
