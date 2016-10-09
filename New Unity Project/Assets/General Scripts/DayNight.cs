d027763f0bec72cb610f25c8ec78a984e3c15fc2
Caelan Douglas <caelan.douglas@outlook.com>
2016-10-08 21:03:42 -0400

    clock not working but whatever

--- New Unity Project/Assets/General Scripts/DayNight.cs
+++ New Unity Project/Assets/General Scripts/DayNight.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 public class DayNight : MonoBehaviour {
 
 	bool TimeIsZero;
+	public Text Clock;
 	public SpriteRenderer DarknessImage;
 	public float secondsInFullDay = 120f;
 	[Range(-1,1)]
 	public float currentTimeOfDay = -1f;
 	public float timeMultiplier = 1f;
+	public float mins;
+	public float hours;
 
 	void Update() {
-
 		if (TimeIsZero == true) {
 			currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
 			if (currentTimeOfDay >= 1) {
@@ -27,5 +29,6 @@ public class DayNight : MonoBehaviour {
 		if (currentTimeOfDay >= 0f) {
 			DarknessImage.color = new Color (0, 0, 0, currentTimeOfDay);
 		} 
+		Clock.text = ("" + hours + ":" + mins);
 	}
 }
