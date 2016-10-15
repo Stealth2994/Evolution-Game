88b8d35b13dd1f767d870e6509034c7d8bf9efe3
Stealth2994 <cnnr252@gmail.com>
2016-10-15 00:08:19 -0400

    those are some pretty gooooooooood optimisations

--- New Unity Project/Assets/Generation Scripts/PoolSystem.cs
+++ New Unity Project/Assets/Generation Scripts/PoolSystem.cs
@@ -8,7 +8,7 @@ public class PoolSystem : MonoBehaviour {
     public bool willGrow = true;
     public int code;
     public List<GameObject> pooledObjects;
-
+    public int listSize;
     void Start()
     {
         //creates the base pool based on poolamount and primes them
@@ -19,40 +19,50 @@ public class PoolSystem : MonoBehaviour {
             obj.SetActive(false);
             pooledObjects.Add(obj);
         }
+        listSize = pooledObjects.Count;
     }
-
+    //ACTIVE INACTIVE
     public GameObject GetPooledObject()
     {
         //looks for one thats not null and grabs it and gives it to the caller
-        for (int i = 0; i < pooledObjects.Count; i++)
+        for (int i = 0; i < listSize; i++)
         {
-            
             if (pooledObjects[i] == null)
             {
-                GameObject obj = (GameObject)Instantiate(pooledObject);
-                obj.SetActive(false);
-                pooledObjects[i] = obj;
-                return pooledObjects[i];
+                pooledObjects.RemoveAt(i);
+                goto done;
             }
             if (!pooledObjects[i].activeInHierarchy)
             {
                 return pooledObjects[i];
             }
+
+            done:;
         }
+        
+
+        
         //if oops we ran out but it can grow just make a new one and give it over
         if (willGrow)
         {
-            GameObject obj = (GameObject)Instantiate(pooledObject);
+        GameObject obj = (GameObject)Instantiate(pooledObject);
             pooledObjects.Add(obj);
+            listSize++;
             return obj;
         }
 
         return null;
     }
+    public void Exterminate(GameObject g)
+    {
+
+        pooledObjects.Remove(g);
+    }
     //Put it back in the pool
     public void RecycleObject(GameObject g)
     {
-        pooledObjects.Add(g);
+        
+        pooledObjects.Insert(0,g);
         g.SetActive(false);
     }
 }
--- New Unity Project/Assets/Generation Scripts/TerrainTileValues.cs
+++ New Unity Project/Assets/Generation Scripts/TerrainTileValues.cs
@@ -8,10 +8,6 @@ public class TerrainTileValues : MonoBehaviour {
     public int bunchChance = 50;
     public float bunchMultiplier = 1.5f;
     public int code = 1000;
-    public bool alive = false;
-    public int unique;
-    public float regenRate = 0;
     public float speed = 1;
-    public bool regen = false;
-    public bool food = false;
+
 }
